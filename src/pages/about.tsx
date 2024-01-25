04efdf8202833b31b31b856e8d32768ae793c685
abduazimxoja <abdusalomov.aa03@gmail.com>
2024-01-25 22:24:58 +0500

    refactor: test using components

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,7 @@
+import MapCard from "@/components/blocks/map/map-card";
+import ArticleCard from "@/components/cards/article/article-card";
+import NewsCard from "@/components/cards/news/news-card";
+import ProductCard from "@/components/cards/product/product-card";
 import Icon from "@/components/icon/icon-list";
 import { FC } from "react";
 
@@ -9,7 +13,36 @@ const About: FC = () => {
       <button className="button button-blue">button</button>
       <button className="button button-yellow">button</button>
       <button className="button button-border">button</button>
-      <Icon name="arrow" className="color-yellow" />
+      <Icon name="youtube" className="color-yellow" />
+      <ProductCard name="" imageUrl="" link="" />
+
+      <NewsCard title="" imageUrl="" text="" link="" date="" />
+
+      <MapCard
+        title={""}
+        address={""}
+        map={{
+          latitude: "",
+          longitude: "",
+        }}
+        phone={""}
+        mail={""}
+        orient={"column"}
+      />
+
+      <MapCard
+        title={""}
+        address={""}
+        map={{
+          latitude: "",
+          longitude: "",
+        }}
+        phone={""}
+        mail={""}
+        orient={"row"}
+      />
+
+      <ArticleCard productCode={5190} text={""} imageUrl={""} link={""} />
     </div>
   );
 };
