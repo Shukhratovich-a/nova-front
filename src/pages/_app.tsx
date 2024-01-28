76b2dddb0af715a2e4b9aa99e223909ed146ce6b
abduazimxoja <abdusalomov.aa03@gmail.com>
2024-01-28 23:52:48 +0500

    feat{ create slider component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import type { AppProps } from "next/app";
 
 import "@/styles/globals.scss";
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import "@/styles/slider/slider.scss";
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
