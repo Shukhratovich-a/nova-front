45b8559e209ca4e0a38605f64ce4153d4718f261
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-02-01 23:23:18 +0500

    feat: add product block

---
+++ src/components/blocks/products/product-block.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@/components/button/button";
+import ProductCard from "@/components/cards/product/product-card";
+import cn from "classnames";
+import { FC } from "react";
+import styles from "./product.module.scss";
+import { ProductBlockProps } from "./products-block.props";
+
+export const ProductBlock: FC<ProductBlockProps> = ({ children, className }) => {
+  return (
+    <div className={cn(styles.wrapper, className, "container")}>
+      <h2>Продукты</h2>
+      <ul className={styles.list}>
+      {children}
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+        <li className={styles.item}>
+          <ProductCard name={""} imageUrl={""} link={""} />
+        </li>
+      </ul>
+      <div className={styles.button}>
+        <Button>Показать больше</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductBlock
---
+++ src/components/blocks/products/products-block.props.ts
@@ -0,0 +1,3 @@
+import { HTMLAttributes } from "react";
+
+export interface ProductBlockProps extends HTMLAttributes<HTMLDivElement> {}
