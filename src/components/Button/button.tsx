4714b107393e2fa910f28ca0693bdf3d8bd7a79c
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-02-01 23:19:08 +0500

    feat: complete button component

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,14 +1,15 @@
-import React from "react";
 import cn from "classnames";
-
-import { ButtonProps } from "./Button.props";
+import React from "react";
 
 import styles from "./Button.module.scss";
+import { ButtonProps } from "./button.props";
 
 export const Button: React.FC<ButtonProps> = ({ className, children, appearance = "accent", ...props }) => {
   return (
-    <button className={cn(styles.button, className)} {...props}>
+    <button className={cn(styles.button, className, styles[appearance])} {...props}>
       {children}
     </button>
   );
 };
+
+export default Button
