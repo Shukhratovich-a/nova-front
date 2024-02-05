01e8ff2694eebdf185bcb2a2575d0979311c60c6
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-02-05 21:37:04 +0500

    refactor: added a second style and added a logo

--- src/layout/Header/Header.component.tsx
+++ src/layout/Header/Header.component.tsx
@@ -5,16 +5,23 @@ import cnBind from "classnames/bind";
 import Link from "next/link";
 import { HeaderProps } from "./Header.props";
 
-import { IconArrowBottom, IconSearch } from "@/assets/icons";
+import { IconSearch } from "@/assets/icons";
+import LogoBlue from "@/assets/images/Logo-blue.png";
+import LogoWhite from "@/assets/images/Logo-white.png";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "./header.module.scss";
 import Navbar from "./navbar/navbar";
-
 const cx = cnBind.bind(styles);
 
 export const Header: React.FC<HeaderProps> = ({ className, children, ...rest }) => {
   const [burgerActive, setBurgerActive] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+
+  // style controller
+  const { pathname } = useRouter();
+  const headerStyleHome = pathname === "/" && styles.headerHome;
+  const getLogoByPath = pathname === "/" || scrolled || burgerActive ? LogoWhite : LogoBlue;
 
   const menuController = () => {
     setBurgerActive(!burgerActive);
@@ -43,18 +50,21 @@ export const Header: React.FC<HeaderProps> = ({ className, children, ...rest }) 
   }, [scrolled]);
 
   return (
-    <header className={cn(className, styles.wrapper, scrolled ? styles.scrolled : "")} {...rest}>
+    <header className={cn(className, styles.wrapper, headerStyleHome, scrolled ? styles.scrolled : "")} {...rest}>
       <div className="container">
         <div className={styles.wrap}>
           <Link className={styles.logo} href="/">
-            <Image src={""} alt="nova logo" width={220} height={62} />
+            <Image src={getLogoByPath} alt="nova logo" width={220} height={62} />
           </Link>
           <Navbar onClick={menuController} active={burgerActive} />
-          <div className={cn(styles.additions, "color-white")}>
+          <div className={cn(styles.additions, cx({ active: burgerActive }))}>
             <IconSearch />
             <div>
-              ru
-              <IconArrowBottom />
+              <select name="" id="">
+                <option value="ru">uz</option>
+                <option value="en">en</option>
+                <option value="kr">ru</option>
+              </select>
             </div>
           </div>
           <button className={cn(styles.burger, burgerActive && cx({ active: true }))} onClick={menuController}>
