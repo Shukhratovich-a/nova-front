d15005228a537b897d67531a173c1facee292d4a
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-02-05 21:49:54 +0500

    refactor: formatting

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,22 @@
-// blocks
+// BLOCKS
 export * from "./blocks/article-slide/article-slide";
-export * from "./blocks/home-intro/home-intro";
+// Contact page blocks
 export * from "./blocks/contacts-map/contacts-map";
+// Home page blocks
+export * from "./blocks/home-intro/home-intro";
 export * from "./blocks/home-products/home-products";
+// Product page blocks
 export * from "./blocks/product-intro/product-intro";
 export * from "./blocks/product-info/product-info";
+export * from "./blocks/product-info/product-info";
+export * from "./blocks/product-related/product-related";
 
-// cards
+// CARDS
 export * from "./cards/article/article-card";
 export * from "./cards/news/news-card";
 export * from "./cards/product/product-card";
 
-// component
+// COMPONENTS
 export * from "./breadcrumbs/breadcrumbs";
 export * from "./button/button";
 export * from "./map/map";
