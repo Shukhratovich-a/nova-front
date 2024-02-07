a38c51556331e324e437c15f16f5a7b847079ad4
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-02-07 16:43:23 +0500

    feat: add banner request

--- src/components/blocks/home-intro/home-intro.props.ts
+++ src/components/blocks/home-intro/home-intro.props.ts
@@ -1,5 +1,6 @@
+import { IBanner } from "@/types/banner.interface";
 import { HTMLAttributes } from "react";
 
 export interface HomeIntroProps extends HTMLAttributes<HTMLDivElement> {
-  data: [{ id: number; title: string; subtitle: string; caption?: string }];
+  banner: IBanner[];
 }
---
+++ src/types/banner.interface.ts
@@ -0,0 +1,7 @@
+export interface IBanner {
+  id: string | number;
+  poster: string;
+  title: string;
+  description: string;
+  subtitle: string;
+}
