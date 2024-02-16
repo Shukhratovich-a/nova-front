25f7e44a8c0a0f4a5479134566b09c4e35468303
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-16 22:07:21 +0500

    refactor: add data fetcher

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,17 +1,52 @@
-import { withLayout } from "@/layout/Layout";
-import { NewsView } from "@/views";
-import Head from "next/head";
 import { FC } from "react";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { ParsedUrlQuery } from "querystring";
 
-const NewsPage: FC = () => {
+import { IPost } from "@/types/post.interface";
+
+import { getAll } from "@/api/post.api";
+
+import { withLayout } from "@/layout/Layout";
+
+import { PostsView } from "@/views";
+
+const PostsPage: FC<PostsPageProps> = ({ posts, total }) => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
-        <title>News</title>
+        <title>{`${t("news")}`}</title>
       </Head>
-      <NewsView />
+
+      <PostsView posts={posts} total={total} />
     </>
   );
 };
 
-export default withLayout(NewsPage);
+export const getServerSideProps: GetServerSideProps<PostsPageProps> = async ({
+  locale,
+  query: { limit },
+}: GetServerSidePropsContext<ParsedUrlQuery>) => {
+  const {
+    data: { data: posts, total },
+  } = await getAll(1, limit ? Number(limit) : 12, locale);
+
+  return {
+    props: {
+      posts,
+      total,
+      ...(await serverSideTranslations(String(locale))),
+    },
+  };
+};
+
+export default withLayout(PostsPage);
+
+interface PostsPageProps extends Record<string, unknown> {
+  posts: IPost[];
+  total: number;
+}
