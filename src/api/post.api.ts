048214139a3d29e03a2d426afa6436aab46e4a27
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-16 21:25:46 +0500

    feat: create post fetcher

---
+++ src/api/post.api.ts
@@ -0,0 +1,16 @@
+import queryString from "query-string";
+import axios from "./axios";
+
+import { IGetAll } from "@/types/response.interface";
+
+import { IPost } from "@/types/post.interface";
+
+export const getAll = (page?: number, limit?: number, language?: string) => {
+  const query = queryString.stringifyUrl({ url: `/news/get-all`, query: { page, limit, language } });
+
+  return axios.get<IGetAll<IPost[]>>(query);
+};
+
+export const getByAlias = async (alias: string) => {
+  return axios.get<IPost>(`/news/get-by-alias/${alias}`);
+};
