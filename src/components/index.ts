404bdf5dcb58a1647515c90443ed282f63959c0b
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-16 22:07:41 +0500

    refactor: change imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,25 +1,30 @@
 // BLOCKS
 export * from "./blocks/article-slide/article-slide";
+
 // Contact page blocks
 export * from "./blocks/contacts-map/contacts-map";
+
 // Home page blocks
 export * from "./blocks/home-intro/home-intro";
 export * from "./blocks/home-products/home-products";
+
 // Product page blocks
 export * from "./blocks/product-intro/product-intro";
 export * from "./blocks/product-info/product-info";
 export * from "./blocks/product-info/product-info";
 export * from "./blocks/product-related/product-related";
-// News page blocks
-export * from "./blocks/news-item/news-item";
+
+// Post page blocks
+export * from "./blocks/post-item/post-item";
+export * from "./blocks/posts-list/posts-list";
 
 // CARDS
 export * from "./cards/article/article-card";
-export * from "./cards/news/news-card";
+export * from "./cards/post/post-card";
 export * from "./cards/product/product-card";
 
 // COMPONENTS
 export * from "./breadcrumbs/breadcrumbs";
-export * from "./button/button";
+export * from "./Button/button";
 export * from "./map/map";
 export * from "./slider/slider";
