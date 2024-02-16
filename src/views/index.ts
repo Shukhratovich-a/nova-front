be769320536926037ad74f8d9ec294a1d97537d2
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-16 22:06:10 +0500

    refactor: change imports

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,5 +2,5 @@ export * from "./Home/Home.view";
 export * from "./Product/Product.view";
 export * from "./Contact/Contact.view";
 export * from "./Media/Media.view";
-export * from "./News/News.view";
-export * from "./News/NewsItem.view";
+export * from "./posts/posts.view";
+export * from "./post/post.view";
