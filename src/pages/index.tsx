54c1d7dd62ee35426ee8b22025a6bad511a71040
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-23 22:52:33 +0500

    refactor: add props for i18n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,42 @@
-import { withLayout } from "@/layout/Layout";
+import { FC } from "react";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import axios from "axios";
+
 import { IBanner } from "@/types/banner.interface";
 
-import { HomeView } from "@/views";
-import axios from "axios";
-import { GetStaticProps } from "next";
-import Head from "next/head";
-import { FC } from "react";
+import { withLayout } from "@/layout/Layout";
 
-const Home: FC<{ banner: IBanner[] }> = (props) => {
+import { HomeView } from "@/views";
+
+const HomePage: FC<HomePageProps> = ({ banners }) => {
   return (
     <>
       <Head>
         <title>Nova</title>
       </Head>
-      <HomeView banner={props.banner} />
+
+      <HomeView banner={banners} />
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await axios.get("http://localhost:3001/banner/get-all?language=ru").catch((error) => error);
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async ({ locale }) => {
+  const {
+    data: { data: banners },
+  } = await axios.get(`http://localhost:3001/banner/get-all?language=${locale}`).catch((error) => error);
 
-  return { props: { banner: res.data || [] } };
+  return {
+    props: {
+      banners,
+      ...(await serverSideTranslations(String(locale))),
+    },
+  };
 };
 
-export default withLayout(Home);
+export default withLayout(HomePage);
+
+export interface HomePageProps extends Record<string, unknown> {
+  banners: IBanner[];
+}
