1206d76d3fb9be03a1cde972452c027f2a182db7
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-25 00:57:15 +0500

    refactor: edit export path

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,4 @@
-export * from "./Home/Home.view";
+export * from "./home/home.view";
 export * from "./Product/Product.view";
 export * from "./Contact/Contact.view";
 export * from "./Media/Media.view";
