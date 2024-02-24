1373c6c05c30eae337512972a79e13a3b316f09d
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-24 18:35:43 +0500

    feat: create menu helper

---
+++ src/helpers/menu.helper.ts
@@ -0,0 +1,32 @@
+export const menu = [
+  {
+    id: 1,
+    route: "/",
+    title: "menu.home",
+  },
+  {
+    id: 2,
+    route: "/about",
+    title: "menu.about",
+  },
+  {
+    id: 3,
+    route: "/category",
+    title: "menu.products",
+  },
+  {
+    id: 4,
+    route: "/media",
+    title: "menu.media",
+  },
+  {
+    id: 5,
+    route: "/news",
+    title: "menu.news",
+  },
+  {
+    id: 6,
+    route: "/contact",
+    title: "menu.contact",
+  },
+].sort((a, b) => a.id - b.id);
