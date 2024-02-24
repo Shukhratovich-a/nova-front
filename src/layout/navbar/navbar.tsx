755d5919df17e056e3c9df790b9e66e3b72e1218
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-25 04:46:36 +0500

    refactor: connect helper

--- src/layout/navbar/navbar.tsx
+++ src/layout/navbar/navbar.tsx
@@ -1,45 +1,28 @@
 import { FC } from "react";
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
 import cn from "classnames";
 
 import { NavbarProps } from "./navbar.props";
 
+import { menu } from "@/helpers/menu.helper";
+
 import styles from "./navbar.module.scss";
 
 export const Navbar: FC<NavbarProps> = ({ className, ...props }) => {
+  const { t } = useTranslation();
+
   return (
     <nav className={cn(styles.nav, className)} {...props}>
       <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link className={cn(styles.link, "nav-link-text")} href="/">
-            Главная
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link className={cn(styles.link, "nav-link-text")} href="/about">
-            О нас
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link className={cn(styles.link, "nav-link-text")} href="/product">
-            Товары
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link className={cn(styles.link, "nav-link-text")} href="/media">
-            Медиа
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link className={cn(styles.link, "nav-link-text")} href="/news">
-            Новости
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link className={cn(styles.link, "nav-link-text")} href="/contact">
-            Контакты
-          </Link>
-        </li>
+        {menu.length &&
+          menu.map((item) => (
+            <li className={styles.item} key={item.id}>
+              <Link className={cn(styles.link, "nav-link-text")} href={item.route}>
+                {t(item.title)}
+              </Link>
+            </li>
+          ))}
       </ul>
     </nav>
   );
