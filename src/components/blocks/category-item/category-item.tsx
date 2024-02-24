a51d00c4ea47e62a2d63c8946dc08663b087fc47
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-25 04:41:43 +0500

    feat: create single category component

---
+++ src/components/blocks/category-item/category-item.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+// import Link from "next/link";
+// import { useTranslation } from "next-i18next";
+import cn from "classnames";
+
+import { CategoryItemProps } from "./category-item.props";
+
+import { CategoryCard, Button } from "@/components";
+
+import styles from "./category-item.module.scss";
+
+export const CategoryItem: FC<CategoryItemProps> = ({ className, category, ...props }) => {
+  const { title, subcategories } = category;
+  // const { t } = useTranslation();
+
+  return (
+    <div className={cn(styles.wrapper, className)} {...props}>
+      <h2>{title}</h2>
+
+      <ul className={cn(styles.list)}>
+        {subcategories &&
+          subcategories.length &&
+          subcategories.map((subcategory) => (
+            <li className={cn(styles.item)} key={subcategory.id}>
+              <CategoryCard category={category} href={`/category/${category.alias}/${subcategory.alias}`} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryItem;
