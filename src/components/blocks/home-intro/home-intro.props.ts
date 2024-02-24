218319572f732167e2ec28ef96855bfcd0c0d4a8
shukhratovich-a <shukhratovich138@gmail.com>
2024-02-25 00:56:36 +0500

    refactor: change field name

--- src/components/blocks/home-intro/home-intro.props.ts
+++ src/components/blocks/home-intro/home-intro.props.ts
@@ -2,5 +2,5 @@ import { IBanner } from "@/types/banner.interface";
 import { HTMLAttributes } from "react";
 
 export interface HomeIntroProps extends HTMLAttributes<HTMLDivElement> {
-  banner: IBanner[];
+  banners: IBanner[];
 }
