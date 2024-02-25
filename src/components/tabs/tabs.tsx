2fa6841f9856a2568db84a89edd80d8069667349
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-02-25 18:48:15 +0500

    fix: fixed the error of displaying hidden tabs

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -76,7 +76,7 @@ export const Tabs: FC = () => {
         i !== actionElements.length - 1 ? visibleElementsWidth + MORE_BTN_RESERVED_WIDTH : visibleElementsWidth;
 
       if (visibleSpaceWidth <= containerVisibleWidth.current && isVisible) {
-        actionEl.classList.add(styles.visible);
+        actionEl.style.display = "flex";
       } else {
         if (isVisible) {
           moreBtnLeftPosition.current = actionElementsWidth.current.slice(0, i).reduce((acc, item) => item + acc, 0);
@@ -84,7 +84,7 @@ export const Tabs: FC = () => {
           isVisible = false;
         }
 
-        actionEl.classList.add(styles.hidden);
+        actionEl.style.display = "none";
 
         actionsMoreData.push(ACTIONS_LIST[i]);
       }
@@ -119,6 +119,10 @@ export const Tabs: FC = () => {
     }
   }, []);
 
+  const getActive = (name: string) => {
+    return toggle === name ? styles.active : "";
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles["actions-wrapper"]}>
@@ -126,8 +130,11 @@ export const Tabs: FC = () => {
           {ACTIONS_LIST.map(({ key, name }) => (
             <div
               key={key}
-              onClick={() => setToggle(name)}
-              className={cn(styles.action, toggle === name ? styles["action-active"] : "")}
+              onClick={() => {
+                setToggle(name);
+                setIsMoreOpen(false);
+              }}
+              className={cn(styles.action, getActive(name))}
             >
               {name}
             </div>
@@ -142,12 +149,15 @@ export const Tabs: FC = () => {
         </div>
       </div>
 
-      <div className={cn(styles["more-options"], isMoreOpen ? styles.visible : styles.hidden)}>
+      <div className={cn(styles["more-options"], isMoreOpen ? "" : styles.hidden)}>
         {actionsMoreList.map(({ key, name }) => (
           <div
             key={key}
-            onClick={() => setToggle(name)}
-            className={cn(styles.action, toggle === name ? styles["action-active"] : "")}
+            onClick={() => {
+              setToggle(name);
+              setIsMoreOpen(false);
+            }}
+            className={cn(styles.action, getActive(name))}
           >
             {name}
           </div>
@@ -156,78 +166,5 @@ export const Tabs: FC = () => {
     </div>
   );
 };
-
-// const [toggle, setToggle] = useState("");
-// const tabsRef = useRef<HTMLUListElement>(null);
-// const [isMobile, setIsMobile] = useState(false);
-// console.log(isMobile);
-
-// useEffect(() => {
-//   const checkMobile = window.innerWidth > 500 ? false : true;
-//   setIsMobile(checkMobile);
-// }, []);
-
-// let isMouseDown: boolean = false;
-// let startX: number = 0;
-// let scrollLeft: number = 0;
-
-// const handleMouseDown = (event: MouseEvent<HTMLUListElement>) => {
-//   isMouseDown = true;
-//   if (!tabsRef.current) return;
-//   startX = event.pageX - tabsRef.current.offsetLeft;
-//   scrollLeft = tabsRef.current.scrollLeft;
-//   console.log(scrollLeft);
-// };
-
-// const handleMouseUp = () => {
-//   isMouseDown = false;
-// };
-
-// const handleMouseMove = (event: MouseEvent<HTMLUListElement>) => {
-//   if (!isMouseDown || !tabsRef.current) return;
-//   event.preventDefault();
-//   const x = event.pageX - tabsRef.current.offsetLeft;
-//   const walk = x - startX; // Задаем скорость прокрутки, умножая разницу на коэффициент
-//   tabsRef.current.scrollLeft = scrollLeft - walk;
-// };
-
-// const getTabHandles = () => {
-//   if (!isMobile) {
-//     return {
-//       onMouseDown: handleMouseDown,
-//       onMouseUp: handleMouseUp,
-//       onMouseLeave: handleMouseUp,
-//       onMouseMove: handleMouseMove,
-//     };
-//   } else return {};
-// };
-
-// return (
-//   <div className={styles.wrapper}>
-//     <ul ref={tabsRef} className={cn(styles.tabs)} {...getTabHandles()}>
-//       {tabsTitle.map((text, index) => {
-//         return (
-//           <li key={index} onClick={() => setToggle(text)} className={toggle === text ? styles["tabs-active"] : ""}>
-//             <p className="nav-link-text">{text}</p>
-//           </li>
-//         );
-//       })}
-//     </ul>
-//     <ul className={styles.content}>
-//       <li className="text-md">
-//         <p>КОЛИЧЕСТВО В ЯЩИКЕ</p>
-//         <p>5</p>
-//       </li>
-//       <li className="text-md">
-//         <p>РАЗМЕРЫ ЯЩИКА</p>
-//         <p>0,153 m³</p>
-//       </li>
-//       <li className="text-md">
-//         <p>ВЕС ЯЩИКА</p>
-//         <p>15,50 kg.</p>
-//       </li>
-//     </ul>
-//   </div>
-// )
 
 export default Tabs;
