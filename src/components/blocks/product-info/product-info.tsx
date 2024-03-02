d68d2c6c85e999a9363ac5246330f6e243bfed67
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-02 22:08:39 +0500

    refactor: connect data

--- src/components/blocks/product-info/product-info.tsx
+++ src/components/blocks/product-info/product-info.tsx
@@ -1,15 +1,18 @@
+import { FC } from "react";
 import cn from "classnames";
-import { FC, useState } from "react";
-import styles from "./product-info.module.scss";
+
 import { ProductInfoProps } from "./product-info.props";
-import Tabs from "@/components/tabs/tabs";
+
+import { Tabs } from "@/components";
+
+import styles from "./product-info.module.scss";
 
 export const ProductInfo: FC<ProductInfoProps> = ({ className, detailCategories, ...props }) => {
   return (
     <div className={cn(styles.wrapper, className)} {...props}>
       <h2 className={styles.title}>Информация о продукте</h2>
 
-      <Tabs />
+      <Tabs tabs={detailCategories} />
     </div>
   );
 };
