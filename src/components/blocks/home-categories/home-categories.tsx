a651d6a15535760db7b6c3f013af89ccb400d959
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-02 18:11:49 +0500

    refactor: changed the product grid to a slider

--- src/components/blocks/home-categories/home-categories.tsx
+++ src/components/blocks/home-categories/home-categories.tsx
@@ -1,13 +1,14 @@
-import { FC } from "react";
-import Link from "next/link";
-import { useTranslation } from "next-i18next";
 import cn from "classnames";
+import { useTranslation } from "next-i18next";
+import Link from "next/link";
+import { FC } from "react";
 
 import { HomeCategoriesProps } from "./home-categories.props";
 
-import { CategoryCard, Button } from "@/components";
+import { Button, CategoryCard, Slider } from "@/components";
 
 import styles from "./home-categories.module.scss";
+import { SwiperSlide } from "swiper/react";
 
 export const HomeCategories: FC<HomeCategoriesProps> = ({ className, categories, ...props }) => {
   const { t } = useTranslation();
@@ -16,14 +17,22 @@ export const HomeCategories: FC<HomeCategoriesProps> = ({ className, categories,
     <div className={cn(styles.wrapper, className)} {...props}>
       <h2>{t("products")}</h2>
 
-      <ul className={cn(styles.list)}>
+      {/* <ul className={cn(styles.list)}>
         {categories.length &&
           categories.map((category) => (
             <li className={cn(styles.item)} key={category.id}>
               <CategoryCard category={category} />
             </li>
           ))}
-      </ul>
+      </ul> */}
+      <Slider mobile={true} type="dynamic" quantity={5}>
+        {categories.length &&
+          categories.map((category) => (
+            <SwiperSlide className={cn(styles.item)} key={category.id}>
+              <CategoryCard category={category} />
+            </SwiperSlide>
+          ))}
+      </Slider>
 
       <div className={cn(styles.button)}>
         <Link href="/category">
