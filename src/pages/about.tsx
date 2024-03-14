c5f52fb56a3dbca8f8a6d24b9f761a3296395adb
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-14 17:15:06 +0500

    refactor: add server side translation props

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,33 @@
-import { withLayout } from "@/layout/layout";
-import AboutView from "@/views/About/about.view";
-import Head from "next/head";
 import { FC } from "react";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-const About: FC = () => {
+import { withLayout } from "@/layout/layout";
+
+import { AboutView } from "@/views";
+
+const AboutPage: FC<AboutPageProps> = () => {
   return (
     <div>
       <Head>
         <title>About</title>
       </Head>
+
       <AboutView />
     </div>
   );
 };
 
-export default withLayout(About);
+export const getStaticProps: GetStaticProps<AboutPageProps> = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(String(locale))),
+    },
+    revalidate: 10,
+  };
+};
+
+export default withLayout(AboutPage);
+
+export interface AboutPageProps extends Record<string, unknown> {}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,11 @@
-import { withLayout } from "@/layout/layout";
-import { ContactView } from "@/views";
-import Head from "next/head";
 import { FC } from "react";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { withLayout } from "@/layout/layout";
+
+import { ContactView } from "@/views";
 
 export const ContactPage: FC = () => {
   return (
@@ -9,9 +13,21 @@ export const ContactPage: FC = () => {
       <Head>
         <title>Contact</title>
       </Head>
+
       <ContactView />
     </>
   );
 };
 
+export const getStaticProps: GetStaticProps<ContactPageProps> = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(String(locale))),
+    },
+    revalidate: 10,
+  };
+};
+
 export default withLayout(ContactPage);
+
+export interface ContactPageProps extends Record<string, unknown> {}
