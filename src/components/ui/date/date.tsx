8cbdc9a05c93c6a3e51e1a28c7a6de23e236883e
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-16 23:15:56 +0500

    refactor: changed the ternary operator to an object

--- src/components/ui/date/date.tsx
+++ src/components/ui/date/date.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
 import cn from "classnames";
-import { format } from "date-fns";
+import { Locale, format } from "date-fns";
 import { ar, enUS, ru, tr } from "date-fns/locale";
 import { useTranslation } from "next-i18next";
+import { FC } from "react";
 
 import { DateTimeProps } from "./date.props";
 
@@ -12,7 +12,10 @@ export const DateTime: FC<DateTimeProps> = ({ className, date, size = "sm", ...p
   const { i18n } = useTranslation();
   const { language } = i18n;
 
-  const locale = language === "ru" ? ru : language === "tr" ? tr : language === "ar" ? ar : enUS;
+  // const locale = language === "ru" ? ru : language === "tr" ? tr : language === "ar" ? ar : enUS;
+
+  const locales: { [key: string]: Locale } = { ru, tr, ar };
+  const locale = locales[language] || enUS;
 
   return (
     <time
