3e2399cc7239f5581ed3e974d47d293c96c50b24
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-17 14:27:37 +0500

    refactor: connect footerMenu and translations

--- src/layout/Footer/Footer.component.tsx
+++ src/layout/Footer/Footer.component.tsx
@@ -1,47 +1,49 @@
+import { FC } from "react";
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
 import cn from "classnames";
-import React from "react";
 
 import { FooterProps } from "./Footer.props";
 
+import { footerMenu } from "@/helpers/menu.helper";
+
+import { Logo } from "@/components";
+
 import { IconFacebook, IconVk, IconYoutube } from "@/assets/icons";
-import LogoBlue from "@/assets/images/Logo-blue.png";
-import Image from "next/image";
-import Link from "next/link";
+
 import styles from "./footer.module.scss";
 
-export const Footer: React.FC<FooterProps> = ({ className }) => {
+export const Footer: FC<FooterProps> = ({ className }) => {
+  const { t } = useTranslation();
+
   return (
-    <footer className={cn(className, styles.wrapper)}>
+    <footer className={cn(styles.wrapper, className)}>
       <div className={styles.social}>
-        <Link href={""}>
+        <Link href={""} target="_blank">
           <IconFacebook className="color-accent" width={40} height={40} />
         </Link>
 
-        <Link href={""}>
+        <Link href={""} target="_blank">
           <IconYoutube className="color-accent" width={40} height={40} />
         </Link>
 
-        <Link href={""}>
+        <Link href={""} target="_blank">
           <IconVk className="color-accent" width={40} height={40} />
         </Link>
       </div>
       <div className={cn(styles.wrap, "container")}>
-        <Image src={LogoBlue} alt="" width={142} height={40} />
+        <Logo width={142} height={40} />
+
         <p className={cn(styles["copyright-info"], "text-md color-gray")}>© NOVA Plastic 2024. Все права защищены.</p>
+
         <nav>
           <ul className={cn(styles["nav-list"], "color-gray")}>
-            <li>
-              <Link href="/about">О нас</Link>
-            </li>
-            <li>
-              <Link href="/career">Карьера</Link>
-            </li>
-            <li>
-              <Link href="/certificate">Сертификаты</Link>
-            </li>
-            <li>
-              <Link href="/contact">Конакты</Link>
-            </li>
+            {!!footerMenu.length &&
+              footerMenu.map((item) => (
+                <li key={item.id}>
+                  <Link href={item.route}>{t(item.title)}</Link>
+                </li>
+              ))}
           </ul>
         </nav>
       </div>
