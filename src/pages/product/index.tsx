d5bd67e206cb942ddc73bee44e2ca9ea522e77e3
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-17 23:23:24 +0500

    refactor: add server side props and connect view

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,3 +1,35 @@
-export const ProductPage = () => {};
+import { FC } from "react";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
-export default ProductPage;
+import { withLayout } from "@/layout/layout";
+
+import { ProductsView } from "@/views";
+
+export const ProductPage: FC<ProductPageProps> = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Head>
+        <title>{`${t("products")} - Nova Plastik`}</title>
+      </Head>
+
+      <ProductsView products={[]} />
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<ProductPageProps> = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(String(locale))),
+    },
+  };
+};
+
+export default withLayout(ProductPage);
+
+interface ProductPageProps extends Record<string, unknown> {}
