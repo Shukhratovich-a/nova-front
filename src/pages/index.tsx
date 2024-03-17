a44d96e6f3f94c62f6d37e6612bee0d660368295
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-17 17:14:52 +0500

    feat: certificate and video props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
-import { FC } from "react";
 import { GetStaticProps } from "next";
-import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
+import { FC } from "react";
 
 import { IBanner } from "@/types/banner.interface";
 import { ICategory } from "@/types/category.interface";
 
 import { getAll as getAllBanners } from "@/api/banner.api";
 import { getAll as getAllCategories } from "@/api/category.api";
+import { getAll as getAllCertificate } from "@/api/certificate.api";
 
 import { withLayout } from "@/layout/layout";
 
+import { ICertificate } from "@/types/certificate.interface";
 import { HomeView } from "@/views";
 
-const HomePage: FC<HomePageProps> = ({ banners, categories }) => {
+const HomePage: FC<HomePageProps> = ({ banners, categories, certificate }) => {
   return (
     <>
       <Head>
         <title>Nova</title>
       </Head>
 
-      <HomeView banners={banners} categories={categories} />
+      <HomeView banners={banners} categories={categories} certificate={certificate} />
     </>
   );
 };
@@ -35,10 +37,13 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async ({ locale }) 
       data: { data: categories },
     } = await getAllCategories({ language: locale, limit: 10 });
 
+    const { data: certificate } = await getAllCertificate({ language: locale });
+
     return {
       props: {
         banners,
         categories,
+        certificate,
         ...(await serverSideTranslations(String(locale))),
       },
       revalidate: 10,
@@ -54,6 +59,6 @@ export default withLayout(HomePage);
 
 export interface HomePageProps extends Record<string, unknown> {
   banners: IBanner[];
-
+  certificate: ICertificate[];
   categories: ICategory[];
 }
--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -1,33 +1,52 @@
-import { FC } from "react";
 import { GetStaticProps } from "next";
-import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
+import { FC } from "react";
 
+import { getAll as getAllVideos } from "@/api/video.api";
+import { getAll as getAllCertificate } from "@/api/certificate.api";
 import { withLayout } from "@/layout/layout";
 
+import { ICertificate } from "@/types/certificate.interface";
+import { IVideo } from "@/types/video.interface";
 import { MediaView } from "@/views";
 
-export const MediaPage: FC<MediaPageProps> = () => {
+export const MediaPage: FC<MediaPageProps> = ({ video, certificate }) => {
   return (
     <>
       <Head>
         <title>Media</title>
       </Head>
 
-      <MediaView />
+      <MediaView video={video} certificate={certificate} />
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps<MediaPageProps> = async ({ locale }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(String(locale))),
-    },
-    revalidate: 10,
-  };
+  try {
+    const { data: video } = await getAllVideos({ language: locale });
+
+    const { data: certificate } = await getAllCertificate({ language: locale });
+
+    return {
+      props: {
+        video,
+        certificate,
+        ...(await serverSideTranslations(String(locale))),
+      },
+      revalidate: 10,
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export default withLayout(MediaPage);
 
-export interface MediaPageProps extends Record<string, unknown> {}
+export interface MediaPageProps extends Record<string, unknown> {
+  video: IVideo[];
+  certificate: ICertificate[];
+}
