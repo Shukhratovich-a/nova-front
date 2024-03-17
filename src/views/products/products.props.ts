6e411dd700b520791b55f3cbde50a919936920c3
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-17 23:22:49 +0500

    feat: create products view

---
+++ src/views/products/products.props.ts
@@ -0,0 +1,7 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+import { IProduct } from "@/types/product.interface";
+
+export interface ProductsProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  products: IProduct[];
+}
