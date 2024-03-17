7bf66c91cb809c436fff910479249495ab3350c8
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-17 17:11:37 +0500

    refactor: add video page

---
+++ src/pages/video/[id].tsx
@@ -0,0 +1,67 @@
+import { FC } from "react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
+
+import { IVideo } from "@/types/video.interface";
+
+import { getAll, getById } from "@/api/video.api";
+
+import { withLayout } from "@/layout/layout";
+
+import { VideoView } from "@/views";
+
+export const VideoPage: FC<VideoPageProps> = ({ video }) => {
+  return (
+    <>
+      <Head>
+        <title>{`${video.title}`}</title>
+      </Head>
+
+      <VideoView video={video} />
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  let paths: string[] = [];
+
+  for (let locale of locales as string[]) {
+    const { data: videos } = await getAll({ limit: 1000000000000, language: locale });
+
+    paths = paths.concat(videos.map((video) => `/${locale}/video/${video.id}`));
+  }
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<VideoPageProps> = async ({ params, locale }) => {
+  if (!params) return { notFound: true };
+
+  const id = params.id as string;
+  if (!id) return { notFound: true };
+
+  try {
+    const { data: video } = await getById(id, { language: locale });
+    if (!video) return { notFound: true };
+
+    return {
+      props: {
+        video,
+        ...(await serverSideTranslations(String(locale))),
+      },
+      revalidate: 10,
+    };
+  } catch {
+    return { notFound: true };
+  }
+};
+
+export default withLayout(VideoPage);
+
+interface VideoPageProps extends Record<string, unknown> {
+  video: IVideo;
+}
