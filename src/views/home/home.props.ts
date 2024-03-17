ed3e0b2b10ca045d81cd8aca4184d5d8376511b4
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-17 17:14:24 +0500

    feat: add certificate block

--- src/views/home/home.props.ts
+++ src/views/home/home.props.ts
@@ -2,8 +2,10 @@ import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 import { IBanner } from "@/types/banner.interface";
 import { ICategory } from "@/types/category.interface";
+import { ICertificate } from "@/types/certificate.interface";
 
 export interface HomeProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   banners: IBanner[];
   categories: ICategory[];
+  certificate: ICertificate[];
 }
--- src/views/home/home.view.tsx
+++ src/views/home/home.view.tsx
@@ -2,9 +2,10 @@ import { FC } from "react";
 
 import { HomeProps } from "./home.props";
 
-import { ArticleSlide, HomeIntro, HomeCategories } from "@/components";
+import { ArticleSlide, HomeCategories, HomeIntro, ProductCard } from "@/components";
+import { SwiperSlide } from "swiper/react";
 
-export const HomeView: FC<HomeProps> = ({ banners, categories }) => {
+export const HomeView: FC<HomeProps> = ({ banners, categories, certificate }) => {
   return (
     <>
       <section>
@@ -14,10 +15,18 @@ export const HomeView: FC<HomeProps> = ({ banners, categories }) => {
       <section className="container">
         <HomeCategories categories={categories} />
       </section>
-      {/* 
+
       <section className="container">
-        <ArticleSlide />
-      </section> */}
+        <ArticleSlide title="Сертификаты">
+          {certificate.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <ProductCard card="certificate" product={item} />
+              </SwiperSlide>
+            );
+          })}
+        </ArticleSlide>
+      </section>
     </>
   );
 };
