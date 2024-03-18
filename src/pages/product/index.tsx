ecbef48a9f5d858933c2b84fe047ab96c1553a40
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-18 22:38:15 +0500

    refactor: connect api

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -4,11 +4,15 @@ import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 
+import { IProduct } from "@/types/product.interface";
+
+import { search } from "@/api/product.api";
+
 import { withLayout } from "@/layout/layout";
 
 import { ProductsView } from "@/views";
 
-export const ProductPage: FC<ProductPageProps> = () => {
+export const ProductPage: FC<ProductPageProps> = ({ products, total }) => {
   const { t } = useTranslation();
 
   return (
@@ -17,14 +21,20 @@ export const ProductPage: FC<ProductPageProps> = () => {
         <title>{`${t("products")} - Nova Plastik`}</title>
       </Head>
 
-      <ProductsView products={[]} />
+      <ProductsView products={products} total={total} />
     </>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<ProductPageProps> = async ({ locale }) => {
+export const getServerSideProps: GetServerSideProps<ProductPageProps> = async ({ locale, query: { q, limit } }) => {
+  const {
+    data: { data: products, total },
+  } = await search(q as string, { language: locale, limit: limit ? Number(limit) : 10 });
+
   return {
     props: {
+      products,
+      total,
       ...(await serverSideTranslations(String(locale))),
     },
   };
@@ -32,4 +42,7 @@ export const getServerSideProps: GetServerSideProps<ProductPageProps> = async ({
 
 export default withLayout(ProductPage);
 
-interface ProductPageProps extends Record<string, unknown> {}
+interface ProductPageProps extends Record<string, unknown> {
+  products: IProduct[];
+  total: number;
+}
