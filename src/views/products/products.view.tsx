c2d46b971f364387dd1005846387974feef4a669
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-18 22:37:59 +0500

    refactor: connect products list

--- src/views/products/products.view.tsx
+++ src/views/products/products.view.tsx
@@ -1,19 +1,22 @@
 import { FC } from "react";
+import cn from "classnames";
 
 import { ProductsProps } from "./products.props";
 
-import { Breadcrumbs, SubcategoryItem } from "@/components";
+import { Breadcrumbs, ProductsList } from "@/components";
 
 import styles from "./category.module.scss";
 
-export const ProductsView: FC<ProductsProps> = ({ products }) => {
+export const ProductsView: FC<ProductsProps> = ({ className, products, total, ...props }) => {
   return (
-    <div className="main-margin container">
+    <div className={cn("main-margin", "container", className)} {...props}>
       <section>
         <Breadcrumbs urlList={[]} mb="10px" />
       </section>
 
-      <section></section>
+      <section>
+        <ProductsList products={products} total={total} />
+      </section>
     </div>
   );
 };
