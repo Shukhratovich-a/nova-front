f515c28f790c3783827e82b8972e9c3c8ad2926d
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-20 17:10:38 +0500

    feat: create api for about

---
+++ src/api/about.api.ts
@@ -0,0 +1,12 @@
+import queryString from "query-string";
+import axios from "./axios";
+
+import { IGetManyOptions } from "@/types/request.interface";
+import { IGetAll } from "@/types/response.interface";
+import { IAbout } from "@/types/about.interface";
+
+export const getAll = (options?: IGetManyOptions) => {
+  const query = queryString.stringifyUrl({ url: `/about/get-all`, query: { ...options } });
+
+  return axios.get<IGetAll<IAbout[]>>(query);
+};
