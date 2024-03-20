7e97368a2d14f04db22ab3c5e678459bbd1c6e7e
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-20 19:32:47 +0500

    refactor: correct url list

--- src/views/category/category.view.tsx
+++ src/views/category/category.view.tsx
@@ -10,10 +10,12 @@ import styles from "./category.module.scss";
 export const CategoryView: FC<CategoryProps> = ({ category }) => {
   const { t } = useTranslation();
 
+  const urlList = [{ title: t("products"), link: "/category" }];
+
   return (
     <div className="main-margin container">
       <section>
-        <Breadcrumbs mb="10px" urlList={[{ title: t("products"), link: "/category" }]} />
+        <Breadcrumbs mb="10px" urlList={urlList} />
       </section>
 
       <section>
