83ed15945e9006085fff5e2e1b1ec22691da380a
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-20 19:51:02 +0500

    refactor: remove breadcrumbs

--- src/views/videos/videos.view.tsx
+++ src/views/videos/videos.view.tsx
@@ -1,14 +1,12 @@
-import { Breadcrumbs } from "@/components";
-import { VideosList } from "@/components/blocks/videos-list/videos-list";
-import { IVideoCard } from "@/types/video.interface";
 import { FC } from "react";
+
+import { IVideoCard } from "@/types/video.interface";
+
+import { VideosList } from "@/components";
 
 export const VideosView: FC<{ videos: IVideoCard[] }> = ({ videos }) => {
   return (
     <div className="main-margin container">
-      <section>
-        <Breadcrumbs mb="30px" urlList={["video"]} />
-      </section>
       <section>
         <VideosList videos={videos} />
       </section>
