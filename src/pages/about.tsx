2b8600372eafc85db2538762a8bf784981b3af2c
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-20 17:11:25 +0500

    refactor: connect about api

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,11 +3,15 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+import { IAbout } from "@/types/about.interface";
+
+import { getAll } from "@/api/about.api";
+
 import { withLayout } from "@/layout/layout";
 
 import { AboutView } from "@/views";
 
-const AboutPage: FC<AboutPageProps> = () => {
+const AboutPage: FC<AboutPageProps> = ({ abouts }) => {
   return (
     <div>
       <Head>
@@ -20,14 +24,27 @@ const AboutPage: FC<AboutPageProps> = () => {
 };
 
 export const getStaticProps: GetStaticProps<AboutPageProps> = async ({ locale }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(String(locale))),
-    },
-    revalidate: 10,
-  };
+  try {
+    const {
+      data: { data: abouts },
+    } = await getAll({ language: locale });
+
+    return {
+      props: {
+        abouts,
+        ...(await serverSideTranslations(String(locale))),
+      },
+      revalidate: 10,
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export default withLayout(AboutPage);
 
-export interface AboutPageProps extends Record<string, unknown> {}
+export interface AboutPageProps extends Record<string, unknown> {
+  abouts: IAbout[];
+}
