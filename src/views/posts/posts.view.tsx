0913d81d76b9361bf71266f0a993ca53ecb2e01e
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-20 19:50:23 +0500

    refactor: remove breadcrumbs

--- src/views/posts/posts.view.tsx
+++ src/views/posts/posts.view.tsx
@@ -1,16 +1,12 @@
 import { FC } from "react";
 
-import { Breadcrumbs, PostsList } from "@/components";
+import { PostsList } from "@/components";
 
 import { PostsProps } from "./posts.props";
 
 export const PostsView: FC<PostsProps> = ({ posts, total }) => {
   return (
     <div className="main-margin container">
-      <section>
-        <Breadcrumbs urlList={['news']} mb="30px" />
-      </section>
-
       <section>
         <PostsList posts={posts} total={total} />
       </section>
