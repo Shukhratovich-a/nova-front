30d2d5bafb89d808dfa6c487dc1612fd3644c664
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-22 02:02:43 +0500

    refactor: change for test

--- src/api/about.api.ts
+++ src/api/about.api.ts
@@ -7,6 +7,5 @@ import { IAbout } from "@/types/about.interface";
 
 export const getAll = (options?: IGetManyOptions) => {
   const query = queryString.stringifyUrl({ url: `/about/get-all`, query: { ...options } });
-
   return axios.get<IGetAll<IAbout[]>>(query);
 };
