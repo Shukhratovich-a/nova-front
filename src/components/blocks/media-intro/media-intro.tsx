cadd4672b933cba741af8372a08c3d4154a86de5
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-22 01:24:20 +0500

    refactor: connect download and watch links

--- src/components/blocks/media-intro/media-intro.tsx
+++ src/components/blocks/media-intro/media-intro.tsx
@@ -1,11 +1,17 @@
-import { Button, Slider } from "@/components";
-import { DOMAIN } from "@/helpers/api.helper";
-import cn from "classnames";
-import Image from "next/image";
 import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import { SwiperSlide } from "swiper/react";
-import styles from "./media-intro.module.scss";
+import queryString from "query-string";
+import cn from "classnames";
+
+import { DOMAIN } from "@/helpers/api.helper";
+
 import { MediaIntroProps } from "./media-intro.props";
+
+import { Button, Slider } from "@/components";
+
+import styles from "./media-intro.module.scss";
 
 export const MediaIntro: FC<MediaIntroProps> = ({ catalogs }) => {
   const catalogBg = (
@@ -33,19 +39,29 @@ export const MediaIntro: FC<MediaIntroProps> = ({ catalogs }) => {
                   className={styles["catalog-image"]}
                   width={372}
                   height={522}
-                  alt=""
+                  alt={title}
                   src={`${DOMAIN}${poster}`}
                 />
                 <div className={styles.content}>
                   <h2 className={cn("color-accent")}>
                     {title} {year}
                   </h2>
-                  <p className={cn("subtitle-lg color-gray")}>{subtitle}</p>
+
+                  {subtitle && <p className={cn("subtitle-lg color-gray")}>{subtitle}</p>}
+
                   <div className={styles.buttons}>
-                    <Button appearance="yellow">Скачать</Button>
-                    <Button appearance="outlined" onClick={() => window.open(`${DOMAIN}${catalog}`, "_blank")}>
-                      Каталог товаров
-                    </Button>
+                    <Link
+                      href={queryString.stringifyUrl({ url: `${DOMAIN}/file/download-file`, query: { file: catalog } })}
+                    >
+                      <Button appearance="yellow">Скачать</Button>
+                    </Link>
+
+                    <Link
+                      href={queryString.stringifyUrl({ url: `${DOMAIN}/file/get-file`, query: { file: catalog } })}
+                      target="_blank"
+                    >
+                      <Button appearance="outlined">Смотреть</Button>
+                    </Link>
                   </div>
                 </div>
               </div>
