b4a29b3045b30d1333831aec7cda7319b51d2de1
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-27 01:52:32 +0500

    feat: create load more reducer

---
+++ src/reducers/load-more.reducer.ts
@@ -0,0 +1,34 @@
+import { Reducer } from "react";
+
+export type LoadMoreActions<T> = { type: "load" } | { type: "reset"; initialState: T[] };
+
+export interface LoadMoreReducerState<T> {
+  limit: number;
+  allItems: T[];
+  limitedItems: T[];
+}
+
+export interface ILoadMoreReducer<T> extends Reducer<LoadMoreReducerState<T>, LoadMoreActions<T>> {}
+
+export const loadMoreReducer = <T>(
+  state: LoadMoreReducerState<T>,
+  action: LoadMoreActions<T>
+): LoadMoreReducerState<T> => {
+  switch (action.type) {
+    case "load":
+      return {
+        ...state,
+        limitedItems: state.allItems.slice(0, state.limit + 10),
+        limit: state.limit + 10,
+      };
+    case "reset": {
+      return {
+        ...state,
+        limitedItems: state.allItems.slice(0, 10),
+        limit: 10,
+      };
+    }
+    default:
+      throw new Error("Неверный тип сортировки");
+  }
+};
