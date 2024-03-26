94450de97869f57831577711cc0ceaf56d3c2c1b
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-27 02:43:50 +0500

    refactor: add watch more button

--- src/components/blocks/categories-list/categories-list.tsx
+++ src/components/blocks/categories-list/categories-list.tsx
@@ -1,24 +1,42 @@
-import { FC } from "react";
-import Link from "next/link";
+import { FC, useEffect, useReducer } from "react";
 import { useTranslation } from "next-i18next";
 import cn from "classnames";
 
 import { CategoriesListProps } from "./categories-list.props";
+import { ICategory } from "@/types/category.interface";
+
+import { ILoadMoreReducer, loadMoreReducer } from "@/reducers/load-more.reducer";
 
 import { CategoryCard, Button, EmptyList } from "@/components";
 
 import styles from "./categories-list.module.scss";
 
 export const CategoriesList: FC<CategoriesListProps> = ({ className, categories, total, ...props }) => {
+  const [{ limitedItems: limitedCategories, limit }, dispatchSort] = useReducer<ILoadMoreReducer<ICategory>>(
+    loadMoreReducer,
+    {
+      allItems: categories,
+      limitedItems: categories.slice(0, 10),
+      limit: 10,
+    }
+  );
+
   const { t } = useTranslation();
+
+  const loadMore = () => {
+    dispatchSort({ type: "load" });
+  };
+
+  useEffect(() => {
+    dispatchSort({ type: "reset", initialState: categories });
+  }, [categories]);
 
   return (
     <div className={cn(styles.wrapper, className)} {...props}>
       <h2>{t("category")}</h2>
-
-      {categories.length ? (
+      {limitedCategories.length ? (
         <ul className={cn(styles.list)}>
-          {categories.map((category) => (
+          {limitedCategories.map((category) => (
             <li className={cn(styles.item)} key={category.id}>
               <CategoryCard category={category} />
             </li>
@@ -27,12 +45,9 @@ export const CategoriesList: FC<CategoriesListProps> = ({ className, categories,
       ) : (
         <EmptyList translateKey="categories" />
       )}
-
-      {categories.length < total && (
+      {limit < total && (
         <div className={cn(styles.button)}>
-          <Link href="/category">
-            <Button>{t("show-more")}</Button>
-          </Link>
+          <Button onClick={loadMore}>{t("show-more")}</Button>
         </div>
       )}
     </div>
