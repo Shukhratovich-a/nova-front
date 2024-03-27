a399579b806149c560f75019de6c3801d501b280
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-27 13:46:40 +0500

    feat: add hover effect

--- src/layout/navbar/navbar.props.ts
+++ src/layout/navbar/navbar.props.ts
@@ -1,3 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
-export interface NavbarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+export interface NavbarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  headerIsScrolled?: boolean
+}
--- src/layout/navbar/navbar.tsx
+++ src/layout/navbar/navbar.tsx
@@ -1,23 +1,30 @@
-import { FC } from "react";
-import Link from "next/link";
-import { useTranslation } from "next-i18next";
 import cn from "classnames";
+import { useTranslation } from "next-i18next";
+import Link from "next/link";
+import { FC } from "react";
 
 import { NavbarProps } from "./navbar.props";
 
 import { headerMenu } from "@/helpers/menu.helper";
 
+import { useRouter } from "next/router";
 import styles from "./navbar.module.scss";
 
-export const Navbar: FC<NavbarProps> = ({ className, ...props }) => {
+export const Navbar: FC<NavbarProps> = ({ className, headerIsScrolled, ...props }) => {
   const { t } = useTranslation();
+  const { pathname } = useRouter();
+
+  const navbarClasses = cn(className, styles.nav, {
+    [styles["nav-home"]]: pathname === "/",
+    [styles["nav-scroll"]]: headerIsScrolled,
+  });
 
   return (
-    <nav className={cn(styles.nav, className)} {...props}>
+    <nav className={navbarClasses} {...props}>
       <ul className={styles.list}>
         {headerMenu.length &&
           headerMenu.map((item) => (
-            <li className={styles.item} key={item.id}>
+            <li className={cn(styles.item, { [styles["item-active"]]: pathname === item.route })} key={item.id}>
               <Link className={cn(styles.link, "nav-link-text")} href={item.route}>
                 {t(item.title)}
               </Link>
