0476ff58a85073bea992b991f53e72007c6a8a85
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-28 12:06:01 +0500

    refactor: remove home-categories component

--- src/components/blocks/home-categories/home-categories.props.ts
+++
@@ -1,7 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
-
-import { ICategory } from "@/types/category.interface";
-
-export interface HomeCategoriesProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  categories: ICategory[];
-}
