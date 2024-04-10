258b87357c6cf89f81edb302b2b761b6e2d0309f
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-10 15:08:37 +0500

    feat: slider changed to react gallery

--- src/components/blocks/product-intro/product-intro.tsx
+++ src/components/blocks/product-intro/product-intro.tsx
@@ -1,59 +1,31 @@
 import cn from "classnames";
-import Image from "next/image";
 import { FC } from "react";
-import { SwiperSlide } from "swiper/react";
 import { ProductIntroProps } from "./product-intro.props";
-
-import { Slider } from "@/components";
 
 import PdfButtons from "@/components/ui/pdf-buttons/pdf-buttons";
 import { DOMAIN } from "@/helpers/api.helper";
+import ImageGallery from "react-image-gallery";
 import styles from "./product-intro.module.scss";
 
 export const ProductIntro: FC<ProductIntroProps> = ({ className, product, ...props }) => {
   const { code, title, description, mainImage, schemeImage, boxImage } = product;
 
-  const images = [
-    { id: 1, image: mainImage },
-    { id: 2, image: schemeImage },
-    { id: 3, image: boxImage },
-  ];
+  const images = [mainImage, schemeImage, boxImage].filter((url) => !!url);
+
+  const items = images.map((url) => ({ original: `${DOMAIN}${url}`, thumbnail: `${DOMAIN}${url}` }));
 
   return (
     <div className={cn(styles.wrapper, className)} {...props}>
-      <div className={styles.text}>
+      <div className={styles.head}>
         <h1 className="color-accent">{code}</h1>
         <h2>{title}</h2>
       </div>
-
       <div
         className={cn(styles.description, "subtitle-md", "custom-scrollbar")}
         dangerouslySetInnerHTML={{ __html: description }}
       />
-
       <PdfButtons name={code} type="product" className={styles.buttons} />
-
-      <Slider className={cn(styles.gallery)} type="dynamic" quantity={1}>
-        {images.map(({ id, image }) => {
-          if (!image) return "";
-
-          return (
-            <SwiperSlide key={id}>
-              <div className={styles.image}>
-                <Image
-                  className={cn("swiper-slide-image")}
-                  src={`${DOMAIN}${image}`}
-                  alt={title}
-                  width={800}
-                  height={400}
-                  priority
-                  quality={100}
-                />
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Slider>
+      <ImageGallery showNav={false} showPlayButton={false} additionalClass={styles.gallery} items={items} />
     </div>
   );
 };
