1f01b783f06fdd79cfb779b27f9d9fe770a50a84
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-22 21:09:42 +0500

    feat: add localize pdf download

--- src/components/ui/pdf-buttons/pdf-buttons.tsx
+++ src/components/ui/pdf-buttons/pdf-buttons.tsx
@@ -6,8 +6,10 @@ import { FC } from "react";
 import Button from "../button/button";
 import styles from "./pdf-buttons.module.scss";
 import { PdfButtonsProps } from "./pdf-buttons.props";
+import { useTranslation } from "next-i18next";
 
 const PdfButtons: FC<PdfButtonsProps> = ({ name, type, className }) => {
+  const { t } = useTranslation();
   // const [isPDFSupported, setIsPDFSupported] = useState(true);
 
   // useEffect(() => {
@@ -18,17 +20,19 @@ const PdfButtons: FC<PdfButtonsProps> = ({ name, type, className }) => {
     product: {
       download: `${DOMAIN}/file/download-product-file/${name}`,
       preview: `${DOMAIN}/file/get-product-file/${name}`,
+      button: "product-download",
     },
     catalog: {
       download: queryString.stringifyUrl({ url: `${DOMAIN}/file/download-file`, query: { file: name } }),
       preview: queryString.stringifyUrl({ url: `${DOMAIN}/file/get-file`, query: { file: name } }),
+      button: "download",
     },
   };
 
   return (
     <div className={cn(className, styles.buttons)}>
       <Link href={urls[type].download}>
-        <Button appearance="yellow">Скачать</Button>
+        <Button appearance="yellow">{t(urls[type].button)}</Button>
       </Link>
 
       {/* {isPDFSupported && (
