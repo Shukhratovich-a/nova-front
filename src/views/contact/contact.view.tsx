1d9aee63ba58411e0436a2135c0159986b7eaa4e
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-22 20:47:51 +0500

    refactor: remove our factories

--- src/views/contact/contact.view.tsx
+++ src/views/contact/contact.view.tsx
@@ -1,31 +1,35 @@
-import { FC } from "react";
-import { useTranslation } from "next-i18next";
 import cn from "classnames";
+import { useTranslation } from "next-i18next";
+import { FC } from "react";
 
 import { ContactProps } from "./contact.props";
 
-import { ContactsMap, ContactsOurFactories } from "@/components";
+import { ContactsMap } from "@/components";
 
 import styles from "./contact.module.scss";
 
 export const ContactView: FC<ContactProps> = ({ className, centrals, factories, ...props }) => {
   const { t } = useTranslation();
 
+  const mapsList = centrals.concat(factories);
+
   return (
     <div className={cn("main-margin", "container", styles.view, className)} {...props}>
-      {!!centrals.length && (
+      {!!mapsList.length && (
         <section className={styles.section}>
           <h2 className={styles.title}>{t("contacts")}</h2>
 
-          {centrals.map((central) => (
-            <ContactsMap contact={central} orient={"row"} key={central.id} />
-          ))}
+          <div className={styles.list}>
+            {mapsList.map((central) => (
+              <ContactsMap contact={central} orient={"row"} key={central.id} />
+            ))}
+          </div>
         </section>
       )}
-
+      {/* 
       <section className={styles.section}>
         <ContactsOurFactories contacts={factories} />
-      </section>
+      </section> */}
     </div>
   );
 };
