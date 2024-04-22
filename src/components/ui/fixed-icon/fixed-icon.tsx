b8e159181216bd32e7fe339147a01a6dc1c5c097
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-22 22:27:57 +0500

    fix: Property 'url' is missing

--- src/components/ui/fixed-icon/fixed-icon.tsx
+++ src/components/ui/fixed-icon/fixed-icon.tsx
@@ -5,7 +5,7 @@ import { IconWhatsApp } from "@/assets/icons";
 import styles from "./fixed-icon.module.scss";
 import Link from "next/link";
 
-export const FixedIcon: FC<{ url: string }> = ({ url = "https://api.whatsapp.com/send?phone=998933883042" }) => {
+export const FixedIcon: FC<{ url?: string }> = ({ url = "https://api.whatsapp.com/send?phone=998933883042" }) => {
   return (
     <div className={styles.wrapper}>
       <Link href={url} target="_blank" title="WhatsApp" className={styles.icon}>
