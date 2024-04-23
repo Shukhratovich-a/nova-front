4056056c6a55d181c8ebb3e41fe285c627c46f0c
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-23 06:37:18 +0500

    refactor: make poster separately for desktop and mobile phone.

--- src/components/blocks/home-intro/home-intro.tsx
+++ src/components/blocks/home-intro/home-intro.tsx
@@ -1,5 +1,4 @@
 import cn from "classnames";
-import Image from "next/image";
 import { FC } from "react";
 import { SwiperSlide } from "swiper/react";
 
@@ -8,23 +7,25 @@ import { HomeIntroProps } from "./home-intro.props";
 import { Slider } from "@/components";
 
 import { DOMAIN } from "@/helpers/api.helper";
+
 import styles from "./home-intro.module.scss";
 
 export const HomeIntro: FC<HomeIntroProps> = ({ banners }) => {
   return (
     <div className={styles.wrapper}>
       <Slider className={cn(styles.swiper)} type={"full-screen"}>
-        {banners.map(({ id, poster, title, description, subtitle }, index) => {
+        {banners.map(({ id, posterDesktop, posterMobile, title, description, subtitle }, index) => {
           return (
             <SwiperSlide className={cn(styles.slide)} key={id}>
               <div className={cn(styles.background)}>
-                <Image
-                  className="swiper-slide-image"
-                  src={`${DOMAIN}${poster}`}
-                  alt={title}
-                  fill
-                  priority
-                  loading="eager"
+                <div
+                  className={cn(styles.background__desktop)}
+                  style={{ backgroundImage: `url('${DOMAIN}${posterDesktop}')` }}
+                />
+
+                <div
+                  className={cn(styles.background__mobile)}
+                  style={{ backgroundImage: `url('${DOMAIN}${posterMobile ? posterMobile : posterDesktop}')` }}
                 />
               </div>
 
