284e5f209832b33d1cdd6ce218876e48ce95ca89
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-23 06:38:08 +0500

    refactor: edit thumbnail and edit download link

--- src/components/blocks/product-intro/product-intro.tsx
+++ src/components/blocks/product-intro/product-intro.tsx
@@ -4,15 +4,23 @@ import { ProductIntroProps } from "./product-intro.props";
 
 import PdfButtons from "@/components/ui/pdf-buttons/pdf-buttons";
 import { DOMAIN } from "@/helpers/api.helper";
-import ImageGallery from "react-image-gallery";
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
 import styles from "./product-intro.module.scss";
+import { useTranslation } from "next-i18next";
 
 export const ProductIntro: FC<ProductIntroProps> = ({ className, product, ...props }) => {
+  const { i18n } = useTranslation();
   const { code, title, description, mainImage, schemeImage, boxImage } = product;
 
   const images = [mainImage, schemeImage, boxImage].filter((url) => !!url);
 
-  const items = images.map((url) => ({ original: `${DOMAIN}${url}`, thumbnail: `${DOMAIN}${url}` }));
+  const items: ReactImageGalleryItem[] = images.map((url) => ({
+    original: `${DOMAIN}${url}`,
+    thumbnail: `${DOMAIN}${url}`,
+    thumbnailHeight: 100,
+    thumbnailWidth: 100,
+    thumbnailClass: styles.thumbnail,
+  }));
 
   return (
     <div className={cn(styles.wrapper, className)} {...props}>
@@ -24,7 +32,7 @@ export const ProductIntro: FC<ProductIntroProps> = ({ className, product, ...pro
         className={cn(styles.description, "subtitle-md", "custom-scrollbar")}
         dangerouslySetInnerHTML={{ __html: description }}
       />
-      <PdfButtons name={code} type="product" className={styles.buttons} />
+      <PdfButtons name={code + "-" + i18n.language} type="product" className={styles.buttons} />
       <ImageGallery showNav={false} showPlayButton={false} additionalClass={styles.gallery} items={items} />
     </div>
   );
