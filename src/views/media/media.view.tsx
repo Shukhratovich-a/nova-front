0e4205a0aa9d1edff71ebe88773f1a412bf3e0fb
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-11 21:16:44 +0500

    refactor: instead of media intro, I put a carousel of catalogs

--- src/views/media/media.view.tsx
+++ src/views/media/media.view.tsx
@@ -1,12 +1,13 @@
-import { FC } from "react";
-import { useTranslation } from "next-i18next";
-import { SwiperSlide } from "swiper/react";
 import cn from "classnames";
+import { useTranslation } from "next-i18next";
+import { FC } from "react";
+import { SwiperSlide } from "swiper/react";
 
 import { MediaProps } from "./media.props";
 
-import { ProductCard, Slider, MediaIntro } from "@/components";
+import { ProductCard, Slider } from "@/components";
 
+import PdfButtons from "@/components/ui/pdf-buttons/pdf-buttons";
 import styles from "./media.module.scss";
 
 export const MediaView: FC<MediaProps> = ({ video, certificate, catalogs }) => {
@@ -14,9 +15,25 @@ export const MediaView: FC<MediaProps> = ({ video, certificate, catalogs }) => {
 
   return (
     <div className="main-margin">
-      {!!catalogs?.length && (
+      {/* {!!catalogs?.length && (
         <section>
           <MediaIntro catalogs={catalogs} />
+        </section>
+      )} */}
+      {!!catalogs.length && (
+        <section className={cn(styles["block-mb"], "container")}>
+          <h2 className={cn(styles.title, "color-accent")}>{t("catalogs")}</h2>
+
+          <Slider type={"dynamic"} quantity={4} title={t("catalogs")}>
+            {catalogs.map((item) => {
+              return (
+                <SwiperSlide className={styles.catalog} key={item.id}>
+                  <ProductCard card="catalog" product={item} />
+                  <PdfButtons name={item.catalog} type="catalog" className={styles.pdfButton} />
+                </SwiperSlide>
+              );
+            })}
+          </Slider>
         </section>
       )}
 
