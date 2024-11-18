96ddf893ee4608e6a566643bc495fad1b24045b3
shukhratovich-a <shukhratovich138@gmail.com>
2024-11-18 23:38:25 +0500

    refactor: correct fixed icon

--- src/components/ui/fixed-icon/fixed-icon.tsx
+++ src/components/ui/fixed-icon/fixed-icon.tsx
@@ -5,7 +5,7 @@ import { IconWhatsApp } from "@/assets/icons";
 import styles from "./fixed-icon.module.scss";
 import Link from "next/link";
 
-const whatsappPhone = process?.env?.WHATSAPP_PHONE;
+const whatsappPhone = process?.env?.NEXT_PUBLIC_WHATSAPP_PHONE;
 export const FixedIcon: FC<{ url?: string }> = ({ url = `https://api.whatsapp.com/send?phone=${whatsappPhone}` }) => {
   return (
     <div className={styles.wrapper}>
