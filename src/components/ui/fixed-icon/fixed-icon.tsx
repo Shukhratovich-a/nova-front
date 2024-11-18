cef913ff3850b213fa37ca34ed08094fb107dede
shukhratovich-a <shukhratovich138@gmail.com>
2024-11-18 23:49:30 +0500

    refactor: correct link

--- src/components/ui/fixed-icon/fixed-icon.tsx
+++ src/components/ui/fixed-icon/fixed-icon.tsx
@@ -6,7 +6,7 @@ import styles from "./fixed-icon.module.scss";
 import Link from "next/link";
 
 const whatsappPhone = process?.env?.NEXT_PUBLIC_WHATSAPP_PHONE;
-export const FixedIcon: FC<{ url?: string }> = ({ url = `https://api.whatsapp.com/send?phone=${whatsappPhone}` }) => {
+export const FixedIcon: FC<{ url?: string }> = ({ url = `https://wa.me/${whatsappPhone}` }) => {
   return (
     <div className={styles.wrapper}>
       <Link href={url} target="_blank" title="WhatsApp" className={styles.icon}>
