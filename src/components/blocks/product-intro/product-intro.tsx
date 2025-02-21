b8b9d8f4df2441d8ebad2366787006b89e7e42d3
Abduazimkhoja <abduazim.work@gmail.com>
2025-02-21 13:12:28 +0500

    refactor: enable image gallery in product intro component

--- src/components/blocks/product-intro/product-intro.tsx
+++ src/components/blocks/product-intro/product-intro.tsx
@@ -5,6 +5,7 @@ import { ProductIntroProps } from "./product-intro.props";
 import PdfButtons from "@/components/ui/pdf-buttons/pdf-buttons";
 import { DOMAIN } from "@/helpers/api.helper";
 import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 import styles from "./product-intro.module.scss";
 import { useTranslation } from "next-i18next";
 
@@ -33,7 +34,7 @@ export const ProductIntro: FC<ProductIntroProps> = ({ className, product, ...pro
         dangerouslySetInnerHTML={{ __html: description }}
       />
       <PdfButtons name={code + "-" + i18n.language} type="product" className={styles.buttons} />
-      {/* <ImageGallery showNav={false} showPlayButton={false} additionalClass={styles.gallery} items={items} /> */}
+      <ImageGallery showNav={false} showPlayButton={false} additionalClass={styles.gallery} items={items} />
     </div>
   );
 };
