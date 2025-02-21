52cddd131612e595d2aaf0a41bd52bbdaa17c194
Abduazimkhoja <abduazim.work@gmail.com>
2025-02-21 13:42:01 +0500

    refactor: optimize image handling in product intro component

--- src/components/blocks/product-intro/product-intro.tsx
+++ src/components/blocks/product-intro/product-intro.tsx
@@ -1,27 +1,33 @@
 import cn from "classnames";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { ProductIntroProps } from "./product-intro.props";
 
 import PdfButtons from "@/components/ui/pdf-buttons/pdf-buttons";
 import { DOMAIN } from "@/helpers/api.helper";
+import { useTranslation } from "next-i18next";
 import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import styles from "./product-intro.module.scss";
-import { useTranslation } from "next-i18next";
 
 export const ProductIntro: FC<ProductIntroProps> = ({ className, product, ...props }) => {
   const { i18n } = useTranslation();
-  const { code, title, description, mainImage, schemeImage, boxImage } = product;
+  const { code, title, description, mainImage, schemeImage, boxImage } = product || {};
 
-  const images = [mainImage, schemeImage, boxImage].filter((url) => !!url);
+  const images: ReactImageGalleryItem[] = useMemo(() => {
+    const images = [mainImage, schemeImage, boxImage].filter((url) => !!url);
 
-  const items: ReactImageGalleryItem[] = images.map((url) => ({
-    original: `${DOMAIN}${url}`,
-    thumbnail: `${DOMAIN}${url}`,
-    thumbnailHeight: 100,
-    thumbnailWidth: 100,
-    thumbnailClass: styles.thumbnail,
-  }));
+    if (images?.length) {
+      return images.map((url) => ({
+        original: `${DOMAIN}${url}`,
+        thumbnail: `${DOMAIN}${url}`,
+        thumbnailHeight: 100,
+        thumbnailWidth: 100,
+        thumbnailClass: styles.thumbnail,
+      }));
+    }
+
+    return [];
+  }, [mainImage, schemeImage, boxImage]);
 
   return (
     <div className={cn(styles.wrapper, className)} {...props}>
@@ -34,7 +40,7 @@ export const ProductIntro: FC<ProductIntroProps> = ({ className, product, ...pro
         dangerouslySetInnerHTML={{ __html: description }}
       />
       <PdfButtons name={code + "-" + i18n.language} type="product" className={styles.buttons} />
-      <ImageGallery showNav={false} showPlayButton={false} additionalClass={styles.gallery} items={items} />
+      <ImageGallery showNav={false} showPlayButton={false} additionalClass={styles.gallery} items={images} />
     </div>
   );
 };
