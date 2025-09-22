101886e5d1d8f49c639af4d3d1ff0bce9d4c9e03
Shukhratovich-a <94617261+Shukhratovich-a@users.noreply.github.com>
2025-09-22 17:23:23 +0500

    refactor: change port

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+
+console.log(port);
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
