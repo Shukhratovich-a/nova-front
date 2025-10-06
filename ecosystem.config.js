cd04eb0b6d96433d08e6cad9775886f8ab62b0ad
shukhratovich-a <shukhratovich138@gmail.com>
2025-10-07 03:09:45 +0500

    refactor: change ports

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,19 @@ module.exports = {
     {
       name: "novaplastik-uz",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3001",
+      args: "start -p 4001",
       env: { NODE_ENV: "production", APP_ENV: "uz" },
     },
     {
       name: "novaplastik-tr",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3002",
+      args: "start -p 4002",
       env: { NODE_ENV: "production", APP_ENV: "tr" },
     },
     {
       name: "novaplastik-eg",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3003",
+      args: "start -p 4003",
       env: { NODE_ENV: "production", APP_ENV: "eg" },
     },
   ],
